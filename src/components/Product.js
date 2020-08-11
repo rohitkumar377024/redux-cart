fb72a3002b66e9276896ce47297b758965379b2b
Rohit <rohitkumar377024@gmail.com>
2020-08-11 23:44:21 +0530

    Added: Add to Cart Functionality (Redux)

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
+import { addToCart } from '../actions';
+import { connect } from 'react-redux';
 
-const Product = ({ id, name, price }) => {
+const Product = ({ id, name, price, addToCart }) => {
   return (
     <div>
       <hr />
       <h2 className="text-lg font-medium">{name}</h2>
       <h3 className="font-semibold">₹{price}</h3>
-      <button className="bg-red-500 hover:bg-red-600 text-white px-8 py-2">
+      <button
+        onClick={() => addToCart({ id, name, price })}
+        className="bg-red-500 hover:bg-red-600 text-white px-8 py-2"
+      >
         Add to Cart
       </button>
     </div>
   );
 };
 
-export default Product;
+export default connect(null, { addToCart })(Product);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,24 @@
+import { ADD_TO_CART, UPDATE_CART_ITEM, DELETE_CART_ITEM } from '../action-types';
+
 const initialState = {
   cart: [
-    { id: 1, name: 'Product 1', price: 1299 },
-    { id: 2, name: 'Product 2', price: 2499 },
-    { id: 3, name: 'Product 3', price: 2799 },
-    { id: 4, name: 'Product 4', price: 4299 },
-    { id: 5, name: 'Product 5', price: 4799 },
-    { id: 6, name: 'Product 6', price: 1099 }
+    // { id: 1, name: 'Product 1', price: 1299 },
+    // { id: 2, name: 'Product 2', price: 2499 },
+    // { id: 3, name: 'Product 3', price: 2799 },
+    // { id: 4, name: 'Product 4', price: 4299 },
+    // { id: 5, name: 'Product 5', price: 4799 },
+    // { id: 6, name: 'Product 6', price: 1099 }
   ]
 };
 
 const rootReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case ADD_TO_CART:
+      const { id, name, price } = action.payload;
+      return { ...state, cart: [...state.cart, { id, name, price }] };
+    default:
+      return state;
+  }
 };
 
 export default rootReducer;
